dab784bece70d764eab7beff40004c5a0d05d2e7
BarKooo <65182408+BarKooo@users.noreply.github.com>
2020-05-15 17:25:03 +0200

    Update base.js

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,3 +1,9 @@
+var baseURL = "https://barkooo.github.io/SocialCounter/";
+if (typeof isCustomPage == 'undefined') isCustomPage = 0;
+if (window.location.hostname.indexOf("local.barkooo.github.io") < 0) {
+    if (window.location.protocol != "https:") window.location.replace("https:" + window.location.href.substring(window.location.protocol.length));
+    if (location.hostname.indexOf("barkooo.github.io") == -1) window.location.replace(baseURL + location.hash);
+    if (window.top !== window.self) window.top.location.replace(window.self.location.href);
 }
 Array.prototype.shuffle = function () {
     var i = this.length,
